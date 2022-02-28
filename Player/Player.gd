b1864ca5e05d9cd77c471682069db3be0bc7af0f
Yagoi23 <jeschollum@gmail.com>
2022-03-01 10:00:17 +1300

    ui

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,7 +10,14 @@ onready var select_draw = $SelectDraw
 onready var camera = $Camera2D 
 var camera_speed = 1
 
+enum state {COMMAND,BUILD}
+var STATE = state.COMMAND
+
+onready var BuildIcon = $UI/BuildingIcon
+onready var CommandIcon = $UI/CommandIcon
+
 func _process(delta):
+	$UI/Label.text = str(STATE)
 	if dragging != true:
 		if Input.is_action_pressed("player_move_up"):
 			position.y -= camera_speed
@@ -28,33 +35,42 @@ func _process(delta):
 		camera_speed = 10
 	if camera_speed < 1:
 		camera_speed = 1
+	if STATE == state.COMMAND:
+		CommandIcon.visible = true
+		BuildIcon.visible = false
+	elif STATE == state.BUILD:
+		CommandIcon.visible = false
+		BuildIcon.visible = true
 
 func _unhandled_input(event):
-	var refrence_position = Vector2.ZERO
-	refrence_position = get_parent().get_node("Player").position
-	#print(refrence_position)
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			for unit in selected:
-				unit.collider.deselect()
-			selected = []
-			dragging = true
-			drag_start = event.position
-		elif dragging:
-			dragging = false
-			select_draw.update_status(drag_start, event.position, dragging)
-			var drag_end = event.position # - Vector2(position.x, global_position.y)
-			#select_rectangle.extents = (drag_end - drag_start)
-			select_rectangle.extents = (drag_end - drag_start)/2# + refrence_position# - (drag_end + drag_start)/2
-			var space = get_world_2d().direct_space_state
-			var query = Physics2DShapeQueryParameters.new()
-			query.set_shape(select_rectangle)
-			query.transform = Transform2D(0, (drag_end + drag_start)/2  + refrence_position)
-			#query.transform = Transform2D(0, (drag_end + drag_start))
-			selected = space.intersect_shape(query)
-			#print(selected)
-			for unit in selected:
-				unit.collider.select()
-	if dragging:
-		if event is InputEventMouseMotion:
-			select_draw.update_status(drag_start, event.position, dragging)
+	if STATE == state.COMMAND:
+		var refrence_position = Vector2.ZERO
+		refrence_position = get_parent().get_node("Player").position
+		#print(refrence_position)
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				for unit in selected:
+					unit.collider.deselect()
+				selected = []
+				dragging = true
+				drag_start = event.position
+			elif dragging:
+				dragging = false
+				select_draw.update_status(drag_start, event.position, dragging)
+				var drag_end = event.position # - Vector2(position.x, global_position.y)
+				#select_rectangle.extents = (drag_end - drag_start)
+				select_rectangle.extents = (drag_end - drag_start)/2# + refrence_position# - (drag_end + drag_start)/2
+				var space = get_world_2d().direct_space_state
+				var query = Physics2DShapeQueryParameters.new()
+				query.set_shape(select_rectangle)
+				query.transform = Transform2D(0, (drag_end + drag_start)/2  + refrence_position)
+				#query.transform = Transform2D(0, (drag_end + drag_start))
+				selected = space.intersect_shape(query)
+				#print(selected)
+				for unit in selected:
+					unit.collider.select()
+		if dragging:
+			if event is InputEventMouseMotion:
+				select_draw.update_status(drag_start, event.position, dragging)
+	if STATE == state.BUILD:
+		print("Build")
--- Units/Unit.gd
+++ Units/Unit.gd
@@ -3,15 +3,19 @@ extends KinematicBody2D
 
 var selected
 onready var SelectedIcon = $SelectedIcon
+#onready var sprite = $ColorRect
 
 func _ready():
 	selected = false
 	SelectedIcon.visible = false
+	#sprite.color = Color(0,0,0)
 
 func select():
 	selected = true
 	SelectedIcon.visible = true
+	#sprite.color = Color(30,150,60)
 
 func deselect():
 	selected = false
 	SelectedIcon.visible = false
+	#sprite.color = Color(0,0,0)
