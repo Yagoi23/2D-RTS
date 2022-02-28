ab0c40cb25380e49aff44e7f0ded10c8962308a4
Yagoi23 <jeschollum@gmail.com>
2022-03-01 09:16:10 +1300

    removed arbitrary code

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,18 +11,19 @@ onready var camera = $Camera2D
 var camera_speed = 1
 
 func _process(delta):
-	if Input.is_action_pressed("player_move_up"):
-		position.y -= camera_speed
-	if Input.is_action_pressed("player_move_down"):
-		position.y += camera_speed
-	if Input.is_action_pressed("player_move_left"):
-		position.x -= camera_speed
-	if Input.is_action_pressed("player_move_right"):
-		position.x += camera_speed
-	if Input.is_action_pressed("increase_camera_speed"):
-		camera_speed += 1
-	if Input.is_action_pressed("decrease_camera_speed"):
-		camera_speed -= 1
+	if dragging != true:
+		if Input.is_action_pressed("player_move_up"):
+			position.y -= camera_speed
+		if Input.is_action_pressed("player_move_down"):
+			position.y += camera_speed
+		if Input.is_action_pressed("player_move_left"):
+			position.x -= camera_speed
+		if Input.is_action_pressed("player_move_right"):
+			position.x += camera_speed
+		if Input.is_action_pressed("increase_camera_speed"):
+			camera_speed += 1
+		if Input.is_action_pressed("decrease_camera_speed"):
+			camera_speed -= 1
 	if camera_speed > 10:
 		camera_speed = 10
 	if camera_speed < 1:
@@ -44,11 +45,11 @@ func _unhandled_input(event):
 			select_draw.update_status(drag_start, event.position, dragging)
 			var drag_end = event.position # - Vector2(position.x, global_position.y)
 			#select_rectangle.extents = (drag_end - drag_start)
-			select_rectangle.extents = (drag_end - drag_start)/2 + refrence_position
+			select_rectangle.extents = (drag_end - drag_start)/2# + refrence_position# - (drag_end + drag_start)/2
 			var space = get_world_2d().direct_space_state
 			var query = Physics2DShapeQueryParameters.new()
 			query.set_shape(select_rectangle)
-			query.transform = Transform2D(0, (drag_end + drag_start)/2 + refrence_position)
+			query.transform = Transform2D(0, (drag_end + drag_start)/2  + refrence_position)
 			#query.transform = Transform2D(0, (drag_end + drag_start))
 			selected = space.intersect_shape(query)
 			#print(selected)
