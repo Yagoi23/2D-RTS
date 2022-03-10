56f3a9f68f32d859023b2fd0fbdef2fc1ebd6dfb
Yagoi23 <jeschollum@gmail.com>
2022-03-11 11:57:58 +1300

    Setup Branch

--- WebPageRedirect.gd
+++ WebPageRedirect.gd
@@ -11,7 +11,7 @@ func _ready():
 	#InputEventMouse.MOUSE_MODE_CAPTURED = 3 
 	OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 	pass # Replace with function body.
-
+#123
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
