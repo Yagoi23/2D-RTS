47659c36ec19c44140b07e8ca7fd80036be29991
Yagoi23 <jeschollum@gmail.com>
2022-03-14 12:40:52 +1300

    modified tileset

--- WebPageRedirect.gd
+++ WebPageRedirect.gd
@@ -9,7 +9,8 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#InputEventMouse.MOUSE_MODE_CAPTURED = 3 
-	OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	#OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	OS.shell_open("http://room14stclair.blogspot.com/2012/09/part-1-leaving-holland-by-jilles-age-7.html")
 	pass # Replace with function body.
 #123
 
