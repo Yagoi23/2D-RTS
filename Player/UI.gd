027ab99d4ecde3e5ac01b23884c46a16f5fb1a60
Yagoi23 <jeschollum@gmail.com>
2022-05-04 14:23:01 +1200

    made player ui disappear

--- Player/UI.gd
+++ Player/UI.gd
@@ -5,16 +5,24 @@ onready var arrowButton = $BottomMenu/ArrowButton
 var arrowButton_position = null
 var Bottom_Menu_up = true
 onready var buttonArrowIcon = $BottomMenu/ArrowButton/ColorRect2/ColorRect3/ArrowIcon
+onready var building_menu = $BottomMenu/BuildingSelectionMenu
 
 func _ready():
 	Bottom_Menu_up = true
 
 func _process(delta):
+	if GlobalInformation.building_preview_hidden == true:
+		building_menu.visible = false
+	else:
+		building_menu.visible = true
 	if Bottom_Menu_up == true:
 		BottomMenu.position.y = 0
 	else:
 		BottomMenu.position.y = 86
-	
+	#if GlobalInformation.player_state == "build":
+	#	building_menu_visible = true
+	#else:
+	#	building_menu_visible = false
 
 func _on_ArrowButton_pressed():
 	#print("pressed")
@@ -24,3 +32,4 @@ func _on_ArrowButton_pressed():
 	else:
 		Bottom_Menu_up = true
 		buttonArrowIcon.flip_v = false
+		
